d61c32a69366f299b54d3e6c611f37abd1a7e5c0
edmar <edmaroliveiraferreira@gmail.com>
2011-05-24 19:29:03 -0300

    add load_cities

--- populate_mongodb.py
+++ populate_mongodb.py
@@ -5,3 +5,21 @@ import geodict_config
 def load_cities():
     reader = csv.reader(open(geodict_config.source_folder+'worldcitiespop.csv', 'rb'))
     
+    for row in reader:
+        try:
+            country = row[0]
+            city = row[1]
+            region_code = row[3]
+            population = row[4]
+            lat = row[5]
+            lon = row[6]
+        except:
+            continue
+
+        if population is '':
+            population = 0
+
+        city = city.strip()
+
+        last_word, index, skipped = pull_word_from_end(city, len(city)-1, False)
+        dict = { "coountry":country,"city":city,"region_code":region_code,"lat":lat,"lon":lon,"last_word":last_word    }
